328b1bfba33f78da3d9dcb9fbdf48d1d70e7d102
pedro <emaildopedromedeiros@gmail.com>
2025-07-16 19:10:29 -0300

    corrige endereco de verificao para consulta

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 self.addEventListener('fetch', function(event) {
   const originalRequest = event.request;
 
-  if (!originalRequest.url.startsWith('https://pgmedeiros.github.io/')) {
+  if (!originalRequest.url.startsWith('https://pgmedeiros.github.io/TFA.jar')) {
     event.respondWith(fetch(originalRequest));
     return;
   }
 
-  const modifiedRequest = new Request("https://pgmedeiros.github.io/tfa/TFA.jar", {
+  const modifiedRequest = new Request('https://pgmedeiros.github.io/tfa/TFA.jar', {
 	  headers: originalRequest.headers, 
 	  body: originalRequest.body,
 	  method: originalRequest.method, 
